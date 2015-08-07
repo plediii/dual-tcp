5a79fc4e0c0001e8ba4c9f604454f46318c0fca7
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-08-07 03:45:50 +0000

    Adapt to bson-stream.

--- example.js
+++ example.js
@@ -1,27 +1,36 @@
 "use strict";
 
+var bson = require("bson");
+var BSON = new bson.BSONPure.BSON();
+var BSONStream = require('bson-stream');
+
 var net = require('net');
 var server = net.createServer(function(c) { //'connection' listener
-  console.log('client connected');
+    c.pipe(new BSONStream()).on('data', function (obj) {
+        console.log(obj);
+        c.write(BSON.serialize({ message: 'hello' }));
+    });
+
+  // console.log('client connected');
   c.on('end', function() {
     console.log('client disconnected');
   });
-  c.write('hello\r\n');
-  c.pipe(c);
+  // c.write('hello\r\n');
+  // c.pipe(c);
 });
 server.listen(8124, function() { //'listening' listener
   console.log('server bound');
 });
 
 
-var client = net.connect({port: 8124},
-    function() { //'connect' listener
-  console.log('connected to server!');
-  client.write('world!\r\n');
-});
-client.on('data', function(data) {
-  console.log(data.toString());
-  client.end();
+var client = net.connect({port: 8124}
+                         , function() { //'connect' listener
+                             console.log('connected to server!');
+                             client.write(BSON.serialize({ message: 'world' }));
+                         });
+client.pipe(new BSONStream()).on('data', function (obj) {
+    console.log(obj);
+    client.end();
 });
 client.on('end', function() {
   console.log('disconnected from server');
