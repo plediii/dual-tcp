fee46381d115c625d3a2d7f02fcd7d345ddfced6
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-08-07 03:28:14 +0000

    Initial commit.
    Copy nodejs TCP examples.

---
+++ example.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var net = require('net');
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('client connected');
+  c.on('end', function() {
+    console.log('client disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
+});
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+});
+
+
+var client = net.connect({port: 8124},
+    function() { //'connect' listener
+  console.log('connected to server!');
+  client.write('world!\r\n');
+});
+client.on('data', function(data) {
+  console.log(data.toString());
+  client.end();
+});
+client.on('end', function() {
+  console.log('disconnected from server');
+});
